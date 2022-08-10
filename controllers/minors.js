4dba8c4bc3ab35c3620d474f1dd61cb7045010c1
James Liu <james_intern@koptional.com>
2022-08-09 21:04:55 -0500

    fix: mongodb please

--- controllers/minors.js
+++ controllers/minors.js
@@ -5,12 +5,12 @@ const MongoClient = require('mongodb').MongoClient;
 
 
 const allMinors = async (req, res) => {
-    const client =await MongoClient.connect(process.env.MONGODB_URI, function(err, dbo) {
+    const client =await  MongoClient.connect(process.env.MONGODB_URI, async function(err, dbo) {
         var db =  dbo.db('minor-database')
         //db.listCollections().toArray().then(cols => console.log("Collections", cols))
         var cursor =  await db.collection('minor-collection').find();
-        await cursor.toArray().then(cols => await res.send(JSON.stringify(cols)));
-        dbo.close();
+        await cursor.toArray().then(cols => res.send(JSON.stringify(cols)));
+        await dbo.close();
     });
 }
 
@@ -24,11 +24,10 @@ const byName = async (req, res ) => {
         var cursor = await db.collection('minor-collection').find();
         await cursor.toArray().then(cols => {minors=JSON.parse(JSON.stringify(cols));
         schoolArr = minors[0][school];
-        await res.send(JSON.stringify(schoolArr));
+        res.send(JSON.stringify(schoolArr));
         });
-        dbo.close();
+        await dbo.close();
     });
-
     //await console.log(minors);
 }
 
